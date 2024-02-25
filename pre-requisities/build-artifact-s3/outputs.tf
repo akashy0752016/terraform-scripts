a16e1800c0a9bae0b5e074b27b1d316aefb81a07
Akash Yadav <akashy075@gmail.com>
2024-02-25 23:51:54 +0530

    Build artifact s3

--- pre-requisities/build-artifact-s3/outputs.tf
+++ pre-requisities/build-artifact-s3/outputs.tf
@@ -1,12 +1,12 @@
-output "build-arifact-bucket" {
-  value = aws_s3_bucket.build-arifact-bucket.bucket
+output "airbus-build-arifact-bucket" {
+  value = aws_s3_bucket.airbus-build-arifact-bucket.bucket
   description = "The name of the s3 state bucket"
 }
-output "build-arifact-bucket-arn" {
-  value = aws_s3_bucket.build-arifact-bucket.arn
+output "airbus-build-arifact-bucket-arn" {
+  value = aws_s3_bucket.airbus-build-arifact-bucket.arn
   description = "The name of the s3 state bucket"
 }
-output "build-arifact-bucket-id" {
-  value = aws_s3_bucket.build-arifact-bucket.id
+output "airbus-build-arifact-bucket-id" {
+  value = aws_s3_bucket.airbus-build-arifact-bucket.id
   description = "The name of the s3 state bucket"
 }
